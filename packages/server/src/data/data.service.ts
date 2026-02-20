4da2db30010ea4316ee48ef599b31da8f4ce45bc
Faiyaz Huq <faiyaz.huq98@gmail.com>
2026-02-19 21:57:23 -0500

    Enhance DataService with logging functionality
    - Introduced a LoggerService in DataService to log data retrieval requests, improving traceability and debugging capabilities.
    - Added logging for the `getData` method to capture the origin and destination country codes being processed.

--- packages/server/src/data/data.service.ts
+++ packages/server/src/data/data.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from "@nestjs/common";
 import { DbService } from "../db/db.service";
+import { LoggerService } from "../logger/logger.service";
 
 @Injectable()
 export class DataService {
+  private readonly logger = new LoggerService(DataService.name);
   constructor(private readonly dbService: DbService) {}
 
   async getData(originCountryCode: string, destinationCountryCode: string) {
+    this.logger.log(
+      `Getting data for ${originCountryCode} -> ${destinationCountryCode}`,
+    );
     return await this.dbService.client.visaRequirement.findUnique({
       where: {
         originCountryCode_destinationCountryCode: {
