48157fa4a8b5078a40c6a0abf2a7487f4f8fd8ab
Faiyaz Huq <faiyaz.huq98@gmail.com>
2026-02-19 20:20:02 -0500

    Add class-transformer dependency and implement global validation pipe
    - Added `class-transformer` to the project for enhanced data transformation capabilities.
    - Integrated a global `ValidationPipe` in the main application to enforce validation rules, ensuring only whitelisted properties are accepted and non-whitelisted properties are forbidden.

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,4 +1,5 @@
 import "dotenv/config";
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
@@ -7,6 +8,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api/v1");
   app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle("CheckVisa API")
