7ef7543072ce1617e9f9c420bafee7d8ee30ac81
Faiyaz Huq <faiyaz.huq98@gmail.com>
2026-08-05 19:17:34 -0400

    Enhance SummaryItemDto with additional properties
    - Added `id` and `originCountryCode` properties to the `SummaryItemDto` class for improved data representation.
    - Removed the `duration` property to streamline the DTO structure.

--- packages/server/src/data/type/SummaryItemDto.ts
+++ packages/server/src/data/type/SummaryItemDto.ts
@@ -1,15 +1,15 @@
 import { ApiProperty } from "@nestjs/swagger";
 
 export class SummaryItemDto {
+  @ApiProperty({ example: "1234567890" })
+  id!: string;
+
+  @ApiProperty({ example: "CA" })
+  originCountryCode!: string;
+
   @ApiProperty({ example: "CA" })
   destinationCountryCode!: string;
 
   @ApiProperty({ example: "VISA_FREE" })
   primaryRequirement!: string;
-
-  @ApiProperty({
-    example: { maxStayDays: 180, description: "180 days" },
-    nullable: true,
-  })
-  duration!: unknown;
 }
